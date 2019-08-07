cac237264b67bbc6829d45978078c09e3e827132
Isaac Parsons <isaacparsons@MacBook-Pro.hitronhub.home>
2019-08-07 15:01:38 -0600

    backend should now server react resources

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/', (req, res)=> {
-    res.send({hi : 'there'})
-})
+// app.get('/', (req, res)=> {
+//     res.send({hi : 'there'})
+// })
+app.get('/', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
 
 
 const PORT = process.env.PORT || 5000
